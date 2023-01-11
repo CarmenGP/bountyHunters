9d3afe45d2d0b0fa7a36ad49cdada05b436d7f99
Verónica Flores Gutiérrez <veflo13@gmail.com>
2023-01-11 15:01:20 +0100

    new branch homeStyle

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -54,7 +54,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create(['name' => 'admin', 'email' =>'admin@admin.com', 'isAdmin' => true]);
 
-        User::factory()->create(['name' => 'use1', 'email' =>'user1@user1.com', 'isAdmin' => false]);
+        User::factory()->create(['name' => 'user1', 'email' =>'user1@user1.com', 'isAdmin' => false]);
 
         //User::factory(5)->create();
     }
