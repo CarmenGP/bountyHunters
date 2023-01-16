fbbb1c7d59645f8b9084af404f276c4b02541816
ZLoln <lolanavalo@gmail.com>
2023-01-16 14:43:33 +0100

    change of social media icons

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,10 +1,10 @@
 <footer>
     <div class="footerContainer">
         <div>&copy;Copyright 2023</div>
-        <img src="https://drive.google.com/file/d/1pE9vKKDS9KW-AgIed72_WB6dfAdv-Unn">
-        <img src="https://drive.google.com/file/d/1nOWfuMIx1rUcxCQxoDJlsXCuBouRWlzM">
-        <img src="https://drive.google.com/file/d/1H09-E4jkJsPXS6dYkZ9KgEcgGjB2293s">
-        <img src="https://drive.google.com/file/d/15yfZ6mcya1NtX-_oukjT6AQpV6UimXTR">
+        <img src="https://drive.google.com/uc?export=view&id=1pE9vKKDS9KW-AgIed72_WB6dfAdv-Unn">
+        <img src="https://drive.google.com/uc?export=view&id=1nOWfuMIx1rUcxCQxoDJlsXCuBouRWlzM">
+        <img src="https://drive.google.com/uc?export=view&id=1H09-E4jkJsPXS6dYkZ9KgEcgGjB2293s">
+        <img src="https://drive.google.com/uc?export=view&id=15yfZ6mcya1NtX-_oukjT6AQpV6UimXTR">
     </div>
     
 </footer>
