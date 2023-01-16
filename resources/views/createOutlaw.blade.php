cd94dcadb66cb88aa994dbd22455533a08ca1bd4
LolaGM <canal030w@gmail.com>
2023-01-16 16:25:07 +0100

    create validation branch for create form

--- resources/views/createOutlaw.blade.php
+++ resources/views/createOutlaw.blade.php
@@ -11,39 +11,39 @@
                             @csrf
                             <div class="col-md-12">
                                 <label for="inputName" class="form-label">Name</label>
-                                <input type="text" name="name" class="form-control" id="inputName" placeholder= "Write the name of the Outlaw">
+                                <input type="text" name="name" class="form-control" id="inputName" placeholder= "Write the name of the Outlaw" required>
                             </div>
                             <div class="col-md-12">
                                 <label for="inputAlias" class="form-label">Alias</label>
-                                <input type="text" name="alias" class="form-control" id="inputAlias" placeholder= "Write the alias of the Outlaw">
+                                <input type="text" name="alias" class="form-control" id="inputAlias" placeholder= "Write the alias of the Outlaw" required>
                             </div>
                             <div class="col-12">
                                 <label for="inputCrime" class="form-label">Crime</label>
-                                <input type="text" name="crime" class="form-control" id="inputCrime" placeholder="Write the crime commited">
+                                <input type="text" name="crime" class="form-control" id="inputCrime" placeholder="Write the crime commited" required>
                             </div>
                             <div class="col-12">
                                 <label for="inputHint" class="form-label">Hint</label>
-                                <input type="text" name="hint" class="form-control" id="inputHint" placeholder="Write the hint related to the outlaw">
+                                <input type="text" name="hint" class="form-control" id="inputHint" placeholder="Write the hint related to the outlaw" required>
                             </div>
                             <div class="col-md-12">
                                 <label for="inputDescription" class="form-label">Description</label>
-                                <textarea type="text-area" name="description" class="form-control" id="inputDescription" placeholder= "Write the description of the Outlaw"></textarea>
+                                <textarea type="text-area" name="description" class="form-control" id="inputDescription" placeholder= "Write the description of the Outlaw" required></textarea>
                             </div>  
                             <div class="col-md-4">
-                                <label for="inputReward" class="form-label">Reward</label>
-                                <input type="text" name="reward" class="form-control" id="inputReward" placeholder= "Write the reward of the Outlaw">
+                                <label for="inputReward" class="form-label">Reward $</label>
+                                <input type="number" name="reward" class="form-control" id="inputReward" placeholder= "Write the reward for the Outlaw" required>
                             </div>
                             <div class="col-md-4">
                                 <label for="inputGang" class="form-label">Gang</label>
-                                <input type="text" name="gang" class="form-control" id="inputGang" placeholder= "Write the number of the people">
+                                <input type="number" max="10" name="gang" class="form-control" id="inputGang" placeholder= "Write the number of the people" required>
                             </div>
                             <div class="col-md-4">
                                 <label for="inputDeadline" class="form-label">Deadline</label>
-                                <input type="text" name="deadline" class="form-control" id="inputDeadline" placeholder= "Write the deadline of the Outlaw">
-                            </div>                                                     
+                                <input type="datetime-local" name="deadline" class="form-control" id="inputDeadline" placeholder= "Write the deadline of the Outlaw" required>
+                            </div>                                             
                             <div class="col-12">
                                 <label for="inputImg" class="form-label">Portrait</label>
-                                <input type="text" name="img" class="form-control" id="inputImg" placeholder= "Write the url adress of the img">
+                                <input type="url" name="img" class="form-control" id="inputImg" placeholder= "Write the url address of the image" required>
                             </div>
                             
                             </div>
