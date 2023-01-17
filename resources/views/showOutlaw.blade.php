871a4dd0b0aa037ae8987513fb9e36ef6c0abaa8
Verónica Flores Gutiérrez <veflo13@gmail.com>
2023-01-17 11:36:53 +0100

    show desing final

--- resources/views/showOutlaw.blade.php
+++ resources/views/showOutlaw.blade.php
@@ -1,33 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
-        <div class="text-center">
-            <div>
-                <h2>{{ $outlaw->name }}</h2>
-                <h3> alias {{ $outlaw->alias }}</h3>
-                <img class="img-thumbnail" src="{{ $outlaw->img}}" alt="{{ $outlaw->name }}">
-                <h3>{{ $outlaw->crime }}</h3>
-                <h4>hint {{ $outlaw->hint }}</h4>
-                <h3>$ {{ $outlaw->reward }}</h3>
-                <p>{{ $outlaw->description }}</p>
-                <h5>gang {{ $outlaw->gang }}</h5>
-                <h4>{{ $outlaw->deadline }}</h4>
+<section>
+    <div class="containerCard">
+        <div class="outlawCardshow">
+            <h2 class="wantedShow"><b>WANTED</b></h2> 
+            <h3 class="aliasShow"> Alias: {{ $outlaw->alias }}</h3>                
+            <h3 class="name">{{ $outlaw->name }}</h3>            
+            <div class="bullet">
+                <img  src="{{ $outlaw->img}}" style="width: 40%; height: 30%;" alt="{{ $outlaw->name }}">
+                <div class="information">
+                    <h5 class="crime">Crime: {{ $outlaw->crime }}</h5>
+                    <h5 >Hint: {{ $outlaw->hint }}</h5>
+                    <h4>${{ $outlaw->reward }}</h4>
+                    <h6> Gang: {{ $outlaw->gang }}</h6>
+                    <h6> {{ $outlaw->deadline }}</h6>
+                </div>
             </div>
-            <div>
-                <button>
-                    <a href="{{ route('join', $outlaw->id)}}">💰 Join</a>
-                </button>
-                <button>
-                    <a href="{{ route('leave', $outlaw->id)}}">🧨 Leave</a>
-                </button>
-            </div>
-            <div class="float-right">
-                <a class="btn btn-primary" href="{{ route('home') }}"> ↩ </a>
+                <p class="description">{{ $outlaw->description }}</p>
+        </div>            
+        <div class="showButtons">
+            <button>
+                <a class="join" href="{{ route('join', $outlaw->id)}}">💰 Join</a>
+            </button>
+            <button>
+                <a class="leave" href="{{ route('leave', $outlaw->id)}}">🧨 Leave</a>
+            </button>    
+            <div class="return">
+                <a class="" href="{{ route('home') }}"> ↩ </a>
             </div>
         </div>
+    </div>
+        
 
             
     </section>
+
+
 
 @endsection
