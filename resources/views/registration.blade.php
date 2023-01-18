0affef6f785e26f76f22d0f22d2a08e5311f1e46
Verónica Flores Gutiérrez <veflo13@gmail.com>
2023-01-18 10:03:18 +0100

    created new branch noneRepeat2

---
+++ resources/views/registration.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>You are in</h2>
+@foreach($outlaw_users as $outlaw_user)
+        <div>
+            <h2 class="wantedShow"><b>WANTED</b></h2>            
+            <div class="bullet">
+                <img  src="{{ $outlaw->img}}" style="width: 20%; height: 30%;" alt="{{ $outlaw->name }}">
+                <div class="information">
+                    <h3> Alias: {{ $outlaw_user->user_id }}</h3>                
+<!--                     <h3>{{ $outlaw->name }}</h3> 
+                    <h5>Hint: {{ $outlaw->hint }}</h5>
+                    <h4>${{ $outlaw->reward }}</h4>
+                    <h6> {{ $outlaw->deadline }}</h6>
+                    <p>{{ $outlaw->description }}</p>
+                </div> -->
+            </div>
+@endforeach
+
+@endsection
